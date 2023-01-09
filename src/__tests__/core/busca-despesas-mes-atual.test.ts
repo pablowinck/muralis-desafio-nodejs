0ef97ecd8ce08410419b2d1cc93fec0703da993a
pablowinck <pablowinck123@gmail.com>
2023-01-09 08:15:28 -0300

    corrige teste

--- src/__tests__/core/busca-despesas-mes-atual.test.ts
+++ src/__tests__/core/busca-despesas-mes-atual.test.ts
@@ -1,12 +1,27 @@
 import { BuscaDespesasMesAtual } from "@usecase/busca-despesas-mes-atual";
 import { DespesaMemoryRepository } from "@outbound/memory/despesa-memory-repository";
 import { Pageable } from "@entity/Pageable";
-import { DespesaFactory } from "../../core/factories/despesa-factory";
+import { Despesa } from "../../core/domain/entity/Despesa";
 
 const repository = new DespesaMemoryRepository();
 const buscaDespesasMesAtual = new BuscaDespesasMesAtual(repository);
+
 it("quando busca, deve retornar lista", async function () {
-  const despesa = DespesaFactory.execute();
+  const despesa = new Despesa(
+    111,
+    20,
+    new Date(),
+    "teste",
+    1,
+    1,
+    "12345678",
+    "rua teste",
+    "123",
+    "casa",
+    "bairro teste",
+    "cidade teste",
+    "estado teste"
+  );
   await repository.save(despesa);
   const result = await buscaDespesasMesAtual.execute(new Pageable(0, 10));
   expect(result.content).toHaveLength(1);
